12f247ce283f3958cfa396402a9b8fbb0ad44a9a
vinyas57 <vinyasbadiyadka@gmail.com>
2025-07-21 12:25:42 +0530

    creating School java file

---
+++ School.java
@@ -0,0 +1,26 @@
+public class School {
+    private int id;
+    private String name;
+    private String location;
+ 
+    public void setName(String nos) {
+        name = nos;
+    }
+    public void setLocation(String nob) {
+        location = nob;
+    }
+	public void setid(int a){
+		id = a;
+	}
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getLocation() {
+        return location;
+    }
+}
+
